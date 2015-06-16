48b28acc9ed81239bbb08df025e89b06c282dc66
juanhenao <judhenaosa@unal.edu.co>
2015-06-15 19:44:15 -0500

    Complete

--- Code/FiltroBlast.pl
+++ Code/FiltroBlast.pl
@@ -5,6 +5,10 @@ use warnings;
 use Data::Dumper;
 
 open my $IN, "< $ARGV[0]";
+open my $OUT1, "> ../Results/SignificativeFilter.out";
+open my $OUT2, "> ../Results/StudentPropose.out";
+
+# CODIGO PRINCIPAL.
 
 my @hits;
 my @seq;
@@ -35,15 +39,73 @@ while (my @file = <$IN>) {
 
 my %align=hash_seq(@hits,@seq);
 
-print "Hits Significativos:\n\n";
+# CODIGO PARA FILTRAR DE ACUERDO A LO REQUERIDO POR EL PARCIAL.
+
+print $OUT1 "Hits Significativos:\n\n";
 foreach my $val (@s_hits) {
-  print $val."\t".$align{$hits[0]}."\n\t".$align{$val}."\n\n";
+  print $OUT1 $val."\t".$align{$hits[0]}."\n\t".$align{$val}."\n\n";
 }
 
-print "Hits NO Significativos:\n\n";
+print $OUT1 "Hits NO Significativos:\n\n";
 foreach my $val (@ns_hits) {
-  print $val."\t".$align{$hits[0]}."\n\t".$align{$val}."\n\n";
+  print $OUT1 $val."\t".$align{$hits[0]}."\n\t".$align{$val}."\n\n";
 }
+
+# CODIGO PARA FILTRAR SEGUN LO PLANTEADO POR EL ESTUDIANTE.
+
+my @gap;
+my @no_gap;
+
+foreach my $key (keys %align) {
+  if ($key !~ /Query/) {
+    if ($align{$key} =~ /-/) {
+      push @gap, $key;
+    } else {
+      push @no_gap, $key;
+    }
+  }
+}
+
+print $OUT2 "PRIMER FILTRO\n\n";
+print $OUT2 "Hits CON Gaps en el alineamiento:\n\n";
+foreach my $num (@gap) {
+  print $OUT2 $num."\t".$align{$hits[0]}."\n\t".$align{$num}."\n\n";
+}
+
+print $OUT2 "Hits SIN Gaps en el alineamiento:\n\n";
+foreach my $num (@no_gap) {
+  print $OUT2 $num."\t".$align{$hits[0]}."\n\t".$align{$num}."\n\n";
+}
+
+my $exclusion = "-" x 10;
+
+my @little;
+my @much;
+
+foreach my $each (keys %align) {
+  if ($each !~ /Query/) {
+    if ($align{$each} =~ $exclusion) {
+      push @much, $each;
+    } else {
+      push @little, $each;
+    }
+  }
+}
+
+print $OUT2 "SEGUNDO FILTRO\n\n";
+print $OUT2 "Hits con MAS de 10 gaps consecutivos:\n\n";
+foreach my $locate (@much) {
+  print $OUT2 $locate."\t".$align{$hits[0]}."\n\t".$align{$locate}."\n\n";
+}
+print $OUT2 "Hits con MENOS de 10 gaps consecutivos:\n\n";
+foreach my $locate (@little) {
+  print $OUT2 $locate."\t".$align{$hits[0]}."\n\t".$align{$locate}."\n\n";
+}
+
+close $IN;
+close $OUT1;
+
+#SUBRUTINAS.
 
 sub hash_seq {
   my (@init_hash)=@_;
